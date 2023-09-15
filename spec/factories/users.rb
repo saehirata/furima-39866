96c14b5c8460e2639565d7ae8b3546775094c307
saehirata <142714849+saehirata@users.noreply.github.com>
2023-09-15 09:43:38 +0900

    ユーザー管理機能の実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.last_name}
+    email {Faker::Internet.email}
+    password {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
+    password_confirmation {password}
+    last_name   { 'やマ田' }
+    first_name  { '太ろウ' }
+    last_name_kana  { 'ヤマダ' }
+    first_name_kana { 'タロウ' }
+    birth_date { Faker::Date.between(from: '1930-01-01', to: '2018-12-31') }
+  end
+end
