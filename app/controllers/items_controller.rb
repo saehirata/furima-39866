7614e1dca00216d1a524cf8bd21700c390452a39
saehirata <142714849+saehirata@users.noreply.github.com>
2023-09-19 17:36:22 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
-  
+
   def index
+    @item = Item.all.order('created_at DESC')
   end
 
   def new
@@ -18,6 +19,7 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
