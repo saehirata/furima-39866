7553c409db44d0aa67fc61b237002b825658125e
saehirata <142714849+saehirata@users.noreply.github.com>
2023-09-27 17:58:33 +0900

    Rubocopによる修正

--- spec/models/order_destination_spec.rb
+++ spec/models/order_destination_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe OrderDestination, type: :model do
-
-   before do
-     @user = FactoryBot.create(:user)
-     @item = FactoryBot.create(:item)
-     @order_destination = FactoryBot.build(:order_destination,user_id: @user.id, item_id: @item.id)
-   end
+  before do
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @order_destination = FactoryBot.build(:order_destination, user_id: @user.id, item_id: @item.id)
+  end
 
   describe '商品購入' do
     context '商品が購入できる場合' do
@@ -36,7 +35,7 @@ RSpec.describe OrderDestination, type: :model do
       it '郵便番号にハイフンが含まれていないと購入できない' do
         @order_destination.postal_code = '1234567'
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include("Postal code is invalid. Enter it as follows (e.g. 123-4567)")
+        expect(@order_destination.errors.full_messages).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
       end
 
       it '都道府県が「---」では購入できない' do
@@ -66,19 +65,19 @@ RSpec.describe OrderDestination, type: :model do
       it '電話番号に半角数字以外が含まれていると購入できない' do
         @order_destination.phone_number = '０１２３４５６７ああ'
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include("Phone number is invalid. Input only number.")
+        expect(@order_destination.errors.full_messages).to include('Phone number is invalid. Input only number.')
       end
 
       it '電話番号が10桁未満だと購入できない' do
         @order_destination.phone_number = '090123456'
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include("Phone number is too short")
+        expect(@order_destination.errors.full_messages).to include('Phone number is too short')
       end
 
       it '電話番号が12桁以上だと購入できない' do
         @order_destination.phone_number = '090123456789'
         @order_destination.valid?
-        expect(@order_destination.errors.full_messages).to include("Phone number is too short")
+        expect(@order_destination.errors.full_messages).to include('Phone number is too short')
       end
 
       it 'user_idが紐づいていなければ購入できない' do
@@ -93,6 +92,5 @@ RSpec.describe OrderDestination, type: :model do
         expect(@order_destination.errors.full_messages).to include("Item can't be blank")
       end
     end
-    
   end
 end
